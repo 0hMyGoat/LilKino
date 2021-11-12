f12b0403bddbc4352b22adc772e9f0de258de5e0
Alex Dvs <alexandre.devos@semifir.com>
2021-11-12 15:18:34 +0100

    feat: routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
+import { MoviesListComponent } from './movies/movies-list/movies-list.component';
+import { MoviesPageComponent } from './movies/movies-page/movies-page.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    MoviesListComponent,
+    MoviesPageComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
